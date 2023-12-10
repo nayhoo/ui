c258f97b7aa8477c3167e41ddb73dc7813cc302c
Tim Kinsman <tkinsm@gmail.com>
2023-12-10 20:26:36 +1030

    chore: test new hex helpers

--- apps/ts-node/src/index.ts
+++ apps/ts-node/src/index.ts
@@ -1,4 +1,9 @@
-import { meaningOfLife } from "@nayhoo/utils";
+import { getContrastTextColor, hexToRgb, meaningOfLife } from "@nayhoo/utils";
 
 // eslint-disable-next-line no-console
 console.log(meaningOfLife);
+
+console.log(hexToRgb("#3498db"));
+console.log(hexToRgb("#ff0000"));
+
+console.log(getContrastTextColor("#00edff"));
