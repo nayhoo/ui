612716f89443f6139eebefca318173ccd61acd4f
Tim Kinsman <tkinsm@gmail.com>
2023-12-12 20:24:42 +1030

    fix: add shadow to primary button

--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -99,6 +99,7 @@ const StyledButton = styled("button", {
       default: {
         color: "$white",
         backgroundColor: "$primary",
+        boxShadow: "$colors$shadow 0px 10px 38px -10px, $colors$shadow 0px 10px 20px -15px",
 
         "@hover": {
           "&:hover": {},
