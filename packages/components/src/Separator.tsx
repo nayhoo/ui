7af1a3e75611c2c0b4e9eda76f97cc7e46f60d40
Tim Kinsman <tkinsm@gmail.com>
2023-12-14 22:23:03 +1030

    fix: separator theme token

--- packages/components/src/Separator.tsx
+++ packages/components/src/Separator.tsx
@@ -2,7 +2,14 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import { styled } from "../stitches.config";
 
 export const Separator = styled(SeparatorPrimitive.Root, {
-  backgroundColor: "$shark",
-  "&[data-orientation=horizontal]": { height: 1, width: "100%" },
-  "&[data-orientation=vertical]": { height: "100%", width: 1 },
+  backgroundColor: "$line",
+
+  "&[data-orientation=horizontal]": {
+    height: 1,
+    width: "100%",
+  },
+  "&[data-orientation=vertical]": {
+    height: "100%",
+    width: 1,
+  },
 });
