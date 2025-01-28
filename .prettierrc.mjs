d0838fd6d02fe8cc76ba5a85a82971d0ad77b05a
timkinsman <tkinsm@gmail.com>
2025-01-28 20:10:01 +1030

    feat(prettier-config): added prettier config package

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+import nayhooPrettierConfig from "@nayhoo/prettier-config";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  ...nayhooPrettierConfig,
+};
+
+export default config;
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+const config = {
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-organize-imports"],
+};
+
+export default config;
