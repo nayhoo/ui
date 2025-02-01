c35c28c8dcc7b42639d0f9f69c622b203fa10829
timkinsman <tkinsm@gmail.com>
2025-02-01 15:04:29 +1030

    chore(utils): removed ui related util functions

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,12 +1,6 @@
 export const meaningOfLife = 42;
 
-export * from "./calculate-luminance";
-
 export * from "./dev-logger";
-
-export * from "./get-contrast-text-color";
-
-export * from "./hex-to-rgb";
 
 export * from "./is-nil";
 
@@ -15,7 +9,5 @@ export * from "./merge";
 export * from "./noop";
 
 export * from "./sleep";
-
-export * from "./s-rgb";
 
 export * from "./to-kebab-case";
