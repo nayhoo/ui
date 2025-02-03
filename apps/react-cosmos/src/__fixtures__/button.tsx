2c59182a1526e1bd369235fb2dd60a210bb7d25c
timkinsman <tkinsm@gmail.com>
2025-02-03 19:59:25 +1030

    chore: added button fixture

--- apps/react-cosmos/src/__fixtures__/button.tsx
+++ apps/react-cosmos/src/__fixtures__/button.tsx
@@ -1,7 +1,47 @@
 import { Button } from "@nayhoo/ui";
+import { useFixtureInput, useFixtureSelect } from "react-cosmos/client";
 
-export default {
-  default: () => <Button>Button</Button>,
-  ghost: () => <Button variant="ghost">Button</Button>,
-  outline: () => <Button variant="outline">Button</Button>,
+// eslint-disable-next-line react/display-name
+export default () => {
+  const [color] = useFixtureSelect("color", {
+    options: ["primary", "secondary", "error"],
+  });
+
+  const [fullWidth] = useFixtureInput("fullWidth", false);
+
+  const [loading, setLoading] = useFixtureInput("loading", false);
+
+  const [shape] = useFixtureSelect("shape", {
+    options: ["square", "pill"],
+  });
+
+  const [size] = useFixtureSelect("size", {
+    options: ["1", "2", "3"],
+  });
+
+  const [variant] = useFixtureSelect("variant", {
+    options: ["default", "ghost", "outline"],
+  });
+
+  const handleOnClick = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <Button
+      color={color}
+      fullWidth={fullWidth}
+      loading={loading}
+      shape={shape}
+      size={size}
+      variant={variant}
+      onClick={handleOnClick}
+    >
+      Press me
+    </Button>
+  );
 };
