5f63720e0e8e7fad22bbbc4b4f989fa9fdebfcd4
timkinsman <tkinsm@gmail.com>
2025-02-08 20:54:47 +1030

    fix(ui): updated panel boxShadow to use theme colors

--- packages/ui/src/recipes/panel.css.ts
+++ packages/ui/src/recipes/panel.css.ts
@@ -5,8 +5,7 @@ export const panelRecipe = recipe({
   base: {
     backgroundColor: theme.semanticColors.surface,
     borderRadius: theme.radii[3],
-    // boxShadow: `${vars.colors.slate.slate8} 0px 10px 38px -10px, ${vars.colors.slate.slate8} 0px 10px 20px -15px`,
-    boxShadow: `rgba(0, 0, 0, 0.12) 0px 10px 38px -10px, rgba(0, 0, 0, 0.12) 0px 10px 20px -15px`,
+    boxShadow: `${theme.colors.slate.slate8} 0px 10px 38px -10px, ${theme.colors.slate.slate8} 0px 10px 20px -15px`,
   },
 
   variants: {},
