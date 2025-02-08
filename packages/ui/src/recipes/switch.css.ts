fa82cdf6d2cd473982c63f673bf276555bbf436c
timkinsman <tkinsm@gmail.com>
2025-02-08 20:51:40 +1030

    fix: passed through size prop to SwitchThumb; tweaked switch styles

--- packages/ui/src/recipes/switch.css.ts
+++ packages/ui/src/recipes/switch.css.ts
@@ -1,71 +1,21 @@
 import { theme } from "@/theme-contracts/theme-contract.css";
 import { RecipeVariants, recipe } from "@vanilla-extract/recipes";
 
-// SwitchThumb
-
-export const switchThumbRecipe = recipe({
-  base: {
-    position: "absolute",
-    left: 0,
-    width: 13,
-    height: 13,
-    backgroundColor: "white",
-    borderRadius: theme.radii.round,
-    boxShadow:
-      "rgba(0, 0, 0, 0.3) 0px 0px 1px, rgba(0, 0, 0, 0.2) 0px 1px 2px;",
-    transition: "transform 100ms cubic-bezier(0.22, 1, 0.36, 1)",
-    transform: "translateX(1px)",
-    willChange: "transform",
-
-    selectors: {
-      '&[data-state="checked"]': {
-        transform: "translateX(11px)",
-      },
-    },
-  },
-
-  variants: {},
-  defaultVariants: {},
-});
-
-// Switch
-
 export const switchRecipe = recipe({
   base: {
     all: "unset",
-    boxSizing: "border-box",
-    userSelect: "none",
-
-    // Reset
-    alignItems: "center",
-    display: "inline-flex",
-    justifyContent: "center",
-    lineHeight: "1",
-    margin: "0",
-    outline: "none",
-    WebkitTapHighlightColor: "rgba(0,0,0,0)",
 
     backgroundColor: theme.colors.slate.slate5,
     borderRadius: theme.radii.full,
     position: "relative",
+    WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
 
     selectors: {
-      "&::before": {
-        boxSizing: "border-box",
-      },
-      "&::after": {
-        boxSizing: "border-box",
-      },
-
       "&:focus": {
         boxShadow: `0 0 0 2px ${theme.colors.slate.slate8}`,
       },
-
       '&[data-state="checked"]': {
-        // backgroundColor: "$blue9",
-        // "&:focus": {
-        //   boxShadow: "0 0 0 2px $colors$blue8",
-        // },
+        backgroundColor: theme.semanticColors.primary.primary9,
       },
     },
   },
@@ -74,19 +24,11 @@ export const switchRecipe = recipe({
     size: {
       "1": {
         width: theme.sizes[5],
-        height: theme.sizes[3],
+        height: theme.sizes[4],
       },
       "2": {
         width: theme.sizes[7],
         height: theme.sizes[5],
-        // [`& ${StyledThumb}`]: {
-        //   width: 21,
-        //   height: 21,
-        //   transform: "translateX(2px)",
-        //   '&[data-state="checked"]': {
-        //     transform: "translateX(22px)",
-        //   },
-        // },
       },
     },
   },
@@ -96,3 +38,46 @@ export const switchRecipe = recipe({
 });
 
 export type SwitchVariants = NonNullable<RecipeVariants<typeof switchRecipe>>;
+
+export const switchThumbRecipe = recipe({
+  base: {
+    all: "unset",
+
+    display: "block",
+    backgroundColor: "white",
+    borderRadius: theme.radii.full,
+    transition: "transform 100ms",
+    transform: "translateX(2px)",
+    willChange: "transform",
+  },
+
+  variants: {
+    size: {
+      "1": {
+        width: theme.sizes[3],
+        height: theme.sizes[3],
+        selectors: {
+          '&[data-state="checked"]': {
+            transform: "translateX(10px)",
+          },
+        },
+      },
+      "2": {
+        width: "20px",
+        height: "20px",
+        selectors: {
+          '&[data-state="checked"]': {
+            transform: "translateX(18px)",
+          },
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    size: "1",
+  },
+});
+
+export type SwitchThumbVariants = NonNullable<
+  RecipeVariants<typeof switchThumbRecipe>
+>;
