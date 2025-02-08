d8ce797321788f79e35863290cc62a48a552517a
timkinsman <tkinsm@gmail.com>
2025-02-08 20:52:19 +1030

    chore(react-cosmos): added Switch fixture

---
+++ apps/react-cosmos/src/__fixtures__/switch.tsx
@@ -0,0 +1,12 @@
+import { Switch } from "@nayhoo/ui";
+import { useFixtureSelect } from "react-cosmos/client";
+
+// eslint-disable-next-line react/display-name
+export default () => {
+  const [size] = useFixtureSelect("size", {
+    options: ["1", "2"],
+  });
+
+  // TODO: styling broken
+  return <Switch size={size} />;
+};
