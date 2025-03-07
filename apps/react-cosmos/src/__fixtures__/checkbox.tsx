5f71a3e813a22e62e5b365877d34c04fad635415
timkinsman <tkinsm@gmail.com>
2025-03-07 14:47:03 +1030

    chore(react-cosmos): added checkbox fixture

---
+++ apps/react-cosmos/src/__fixtures__/checkbox.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from "@nayhoo/ui";
+import { useFixtureSelect } from "react-cosmos/client";
+
+// eslint-disable-next-line react/display-name
+export default () => {
+  const [size] = useFixtureSelect("size", {
+    options: ["1", "2"],
+  });
+
+  return <Checkbox size={size} />;
+};
