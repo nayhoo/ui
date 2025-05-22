8936c16107f15ada8fdc8219cae61f77459be462
timkinsman <tkinsm@gmail.com>
2025-05-22 20:26:09 +0930

    feat: add vite example

---
+++ apps/vite/src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@nayhoo/ui/button";
+import "./App.css";
+
+function App() {
+  return <Button fullWidth={{ initial: true, sm: false }}>Hello world</Button>;
+}
+
+export default App;
